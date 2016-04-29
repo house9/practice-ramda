c3319836df9f43834135fa71e328f473b32e0e26
Jesse House <jesse.house@codingzeal.com>
2016-04-28 22:09:54 -0700

    Initial commit with basic mocha es6 setup

---
+++ dist/index.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var _chai = require('chai');
+
+describe('testing', function () {
+  it('does things', function () {
+    var actual = true;
+    var expected = true;
+    (0, _chai.expect)(actual).to.eql(expected);
+  });
+});
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import { expect } from 'chai'
+
+describe('testing', () => {
+  it('does things', () => {
+    const actual = true
+    const expected = true
+    expect(actual).to.eql(expected)
+  })
+})
